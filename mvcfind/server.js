35b0350db4e57be792179d5389bb9aaae6fb6ff8
Raymond SO <rso@ouhk.edu.hk>
2015-10-25 07:10:27 +0800

    MVC example using Express EJS

---
+++ mvcfind/server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get("/show", function(req,res) {
+	var fields = filterResult(req.query.id);
+	mongoose.connect(MONGODBURL, function(err) {
+		assert.equal(err,null);
+		var Kitten = mongoose.model('Kitten', kittySchema);
+		Kitten.find({},fields,function(err,results) {
+			assert.equal(err,null);
+			db.close();
+			res.render("kitties", {kitties: results});
+			res.end();
+		})
+	});
+});
+
+var mongoose = require('mongoose');
+var assert = require('assert');
+var MONGODBURL = 'mongodb://localhost/test';
+
+var kittySchema = require('./models/kitty');
+var db = mongoose.connection;
+
+// Controller
+function filterResult(id) {
+	fields = (id == "admin") ? "name age -_id" : "name -_id";
+	return(fields);
+}
+
+app.listen(process.env.PORT || 8099);
