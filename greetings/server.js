44e60551e7af8363215474eba01717ff9427ac6b
Raymond SO <rso@ouhk.edu.hk>
2015-10-25 06:47:07 +0800

    Simple EJS example

---
+++ greetings/server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get("/", function(req,res) {
+	res.render("welcome", {greetMsg: greetingMessage()});
+});
+
+function greetingMessage() {
+    var dateToday = new Date();
+    var theHour = dateToday.getHours();
+    if (theHour > 18) {
+        gMessage = 'Good Evening';
+    } else if (theHour > 12) {
+        gMessage = 'Good Afternoon';
+    } else {
+        gMessage = 'Good Morning';
+    }
+    return gMessage;
+}
+
+app.listen(process.env.PORT || 8099);
