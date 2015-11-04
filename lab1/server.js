72f5afe172511a5fce783e7ade5d1179d0b54328
Raymond SO <rso@ouhk.edu.hk>
2015-11-04 13:15:01 +0800

    fixed bugs

--- lab1/server.js
+++ lab1/server.js
@@ -4,25 +4,24 @@ var mongoose = require('mongoose');
 var assert = require('assert');
 var MONGODBURL = 'mongodb://localhost/test';
 var kittySchema = require('./models/kitty');
-var db = mongoose.connection;
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 
 app.get('/', function(req,res) {
-	res.sendFile('index.html');  // serve static files
+    res.sendFile(__dirname + '/public/index.html');  // serve static files
 });
 
 app.get('/new', function(req,res) {
 	res.write('<html><body><h1>Under Construction</h1>');
-	res.write('<a href="/">Go Home</a>');
+	res.write('<a href="/">Go Home</a></body></html>');
 	res.end();
 });
 
 app.get('/show', function(req,res) {
 	res.write('<html><body><h1>Under Construction</h1>');
-	res.write('<a href="/">Go Home</a>');
+	res.write('<a href="/">Go Home</a></body></html>');
 	res.end();
 });
 
