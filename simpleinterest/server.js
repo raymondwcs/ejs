d2b70c8af4a7f651fb7fde82e8b6f856da052f8b
Raymond So <rso@ouhk.edu.hk>
2019-11-06 07:56:49 +0800

    updated

--- simpleinterest/server.js
+++ simpleinterest/server.js
@@ -3,6 +3,15 @@ var app = express();
 
 app.set('view engine', 'ejs');
 
+class SimpleInterest {
+   constructor(p, r, t) {
+      this.principal = p;
+      this.rate = r;
+      this.time = t;
+      this.interest = p * r * t;
+   }
+}
+
 app.get('/simpleinterest', (req,res) => {
    let obj = new SimpleInterest(req.query.p,req.query.r,req.query.t);
 
@@ -15,12 +24,3 @@ app.get('/simpleinterest', (req,res) => {
 });
 
 app.listen(app.listen(process.env.PORT || 8099));
-
-class SimpleInterest {
-   constructor(p, r, t) {
-      this.principal = p;
-      this.rate = r;
-      this.time = t;
-      this.interest = p * r * t;
-   }
-}
